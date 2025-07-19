f1cb3081ca2c52ac551a762267799d77c203e0dd
Marchionni Martin <tinchomarchi34@gmail.com>
2025-07-19 19:54:44 -0300

    refactor: Hardcode timeZone in request config

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,4 +1,4 @@
-import { getRequestConfig, getTimeZone } from "next-intl/server";
+import { getRequestConfig } from "next-intl/server";
 import { hasLocale } from "next-intl";
 import { getSupportedLanguages } from "@plyaz/translations";
 
@@ -8,11 +8,10 @@ export default getRequestConfig(async ({ requestLocale }) => {
     ? requested
     : "en";
   
-    const timeZone = await getTimeZone();
 
   return {
     locale,
-    timeZone,
+    timeZone: "Europe/London",
     messages: (await import(`../../messages/${locale}.json`)).default,
   };
 });
