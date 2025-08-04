83efb03b6a5b53fc24f4de7696a6584ac2a7aab0
Red <red.pace.dev@gmail.com>
2025-08-04 20:59:32 +0200

    feat: Added env vars for coming soon to be enabled or not

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,7 @@
 import { Box, Section } from "@plyaz/ui";
 import React from "react";
 
+import ComingSoon from "src/components/ComingSoon";
 import EcosystemOverview from "src/components/EcosystemOverview";
 import FeatureCards from "src/components/FeatureCards";
 import HeroSection from "src/components/HeroSection";
@@ -10,6 +11,10 @@ import OurPartners from "src/components/OurPartners";
 import OurTeam from "src/components/OurTeam";
 
 export default function Home(): React.ReactElement {
+  if (process.env.NEXT_PUBLIC_COMING_SOON === "true") {
+    return <ComingSoon />;
+  }
+
   return (
     <Box element="main">
       <HeroSection />
